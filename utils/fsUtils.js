810dca25c79eebe42806ef35510b5f261f9a30e2
Himanshu <himanshu@Himanshus-MacBook-Pro.local>
2021-03-03 17:10:44 +0530

    changes in download directory

--- utils/fsUtils.js
+++ utils/fsUtils.js
@@ -96,16 +96,16 @@ module.exports = {
             activeDownloads[toFile] =  RNFetchBlob
                     .config({
                         fileCache : true,
-                        addAndroidDownloads: {
-                            useDownloadManager: true, // <-- this is the only thing required
-                            // Optional, override notification setting (default to true)
-                            notification: false,
-                            // Optional, but recommended since android DownloadManager will fail when
-                            // the url does not contains a file extension, by default the mime type will be text/plain
-                            // mime: 'text/plain',
-                            mediaScannable: false,
-                            path: tmpFile
-                        }, 
+                        // addAndroidDownloads: {
+                        //     useDownloadManager: true, // <-- this is the only thing required
+                        //     // Optional, override notification setting (default to true)
+                        //     notification: false,
+                        //     // Optional, but recommended since android DownloadManager will fail when
+                        //     // the url does not contains a file extension, by default the mime type will be text/plain
+                        //     // mime: 'text/plain',
+                        //     mediaScannable: false,
+                        //     path: tmpFile
+                        // }, 
                         path: tmpFile
                     })
                     .fetch('GET', fromUrl, headers)
