8c9f3cca7aca3703ab8958737639a45462a79677
Himanshu <himanshu@ip-192-168-0-103.ap-south-1.compute.internal>
2021-03-12 12:37:52 +0530

    pathutils exported

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-'use strict';
+"use strict";
 
-module.exports.CachedImage = require('./CachedImage');
-module.exports.ImageCacheProvider = require('./ImageCacheProvider');
-module.exports.ImageCacheManager = require('./ImageCacheManager');
-module.exports.ImageCachePreloader = require('./ImageCachePreloader');
-module.exports.FsUtils = require('./utils/fsUtils');
+module.exports.CachedImage = require("./CachedImage");
+module.exports.ImageCacheProvider = require("./ImageCacheProvider");
+module.exports.ImageCacheManager = require("./ImageCacheManager");
+module.exports.ImageCachePreloader = require("./ImageCachePreloader");
+module.exports.FsUtils = require("./utils/fsUtils");
+module.exports.PathUtils = require("./utils/pathUtils");
